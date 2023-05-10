12dccf464d4188b8bbac5b514708c2db4c95d677
Jose Guevara <94gjose@gmail.com>
2023-05-10 06:59:34 -0700

    feat: add collision to the world

--- Assets/Scripts/CreateWithCode/Unit1/PlayerController.cs
+++ Assets/Scripts/CreateWithCode/Unit1/PlayerController.cs
@@ -6,16 +6,16 @@ namespace Assets.Scripts.CreateWithCode.Unit1
 {
     public class PlayerController : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] float speed = 20; // meters per second
+
         void Start()
         {
 
         }
 
-        // Update is called once per frame
         void Update()
         {
-            transform.Translate(Vector3.forward);
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed));
         }
     }
 }
