f82debaa90a57f00b09a8384030ffd517dc0d52e
Jose Guevara <94gjose@gmail.com>
2023-05-19 04:20:37 -0700

    feat: complete challenge 2
    make the balls spawn from the top of the screen
    make the player spawn dogs
    destroy the balls on dog contact
    spawn random bals
    random spawn interval

---
+++ Assets/Challenges/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    [SerializeField] private float fireRate = 0.8f;
+
+    private float lastShoot;
+
+    private void Update()
+    {
+        var canShoot = Time.time - lastShoot > fireRate;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canShoot)
+        {
+            lastShoot = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenges/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float minSpawnInterval = 3f;
+    private float maxSpawnInterval = 5f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke(nameof(SpawnRandomBall), startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    private void SpawnRandomBall()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        var randomIndex = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomIndex], spawnPos, ballPrefabs[randomIndex].transform.rotation);
+
+        float randomTime = Random.Range(minSpawnInterval, maxSpawnInterval);
+        Invoke(nameof(SpawnRandomBall), randomTime);
+    }
+}
