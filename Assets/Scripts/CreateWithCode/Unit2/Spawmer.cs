fc6063c348f0bec7ec5d4b6cd9a45fcb805ec943
Jose Guevara <94gjose@gmail.com>
2023-05-18 20:21:25 -0700

    feat: create animal stampede
    create spawn animal
    spawn animal if S is pressed
    randomize the spawn location
    change camera to ortographic

---
+++ Assets/Scripts/CreateWithCode/Unit2/Spawmer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Assets.Scripts.CreateWithCode.Unit2
+{
+    public class Spawmer : MonoBehaviour
+    {
+        [Serializable]
+        public struct SpawmerBounds
+        {
+            public float min;
+            public float max;
+        }
+
+        [SerializeField] private SpawmerBounds boundX;
+        [SerializeField] private SpawmerBounds boundY;
+
+        [SerializeField] private List<GameObject> prefabs = new();
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                var randomPosition = new Vector3(
+                    UnityEngine.Random.Range(boundX.min, boundX.max),
+                    0,
+                    UnityEngine.Random.Range(boundY.min, boundY.max)
+                    );
+                var randomAnimal = prefabs[UnityEngine.Random.Range(0, prefabs.Count)];
+                Instantiate(randomAnimal, randomPosition, randomAnimal.transform.rotation);
+            }
+        }
+    }
+}
