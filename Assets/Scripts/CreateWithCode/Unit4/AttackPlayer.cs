56e884804476c0f2e29496c9556e783ef75bee91
Jose Guevara <94gjose@gmail.com>
2023-08-08 09:54:39 -0400

    feat(unit 4): add harder enemies

--- Assets/Scripts/CreateWithCode/Unit4/AttackPlayer.cs
+++ Assets/Scripts/CreateWithCode/Unit4/AttackPlayer.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace CreateWithCode.Unit4
 {
     public class AttackPlayer : MonoBehaviour
     {
         [SerializeField] private float speed = 3.0f;
-
+        [SerializeField] private bool tackle;
         private Rigidbody _rigidbody;
         private GameObject _player;
+        private Vector3 _lookupDirection;
 
         private void Start()
         {
             _player = GameObject.Find("Player");
             _rigidbody = GetComponent<Rigidbody>();
+
+            LookPlayer();
+        }
+
+        private void LookPlayer()
+        {
+            _lookupDirection = (_player.transform.position - transform.position).normalized;
         }
 
         private void Update()
         {
-            var lookupDirection = (_player.transform.position - transform.position).normalized;
-            _rigidbody.AddForce(lookupDirection * speed);
+            if (!tackle)
+            {
+                LookPlayer();
+            }
+
+            _rigidbody.AddForce(_lookupDirection * speed);
         }
     }
 }
--- Assets/Scripts/CreateWithCode/Unit4/EnemySpawner.cs
+++ Assets/Scripts/CreateWithCode/Unit4/EnemySpawner.cs
@@ -5,17 +5,28 @@ using Random = UnityEngine.Random;
 
 namespace CreateWithCode.Unit4
 {
+    [Serializable]
+    internal struct CustomRange
+    {
+        public int min;
+        public int max;
+    }
     public class EnemySpawner : MonoBehaviour
     {
         [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private GameObject superEnemyPrefab;
         [SerializeField] private int spawnRange = 9;
         [SerializeField] private int spawnHeight = 2;
+        [SerializeField] private CustomRange spawnSuperEnemyRange;
         private int _enemyNumbers = 1;
 
         private void Start()
         {
             // InvokeRepeating(nameof(SpawnEnemy), 0.5f, 4f);
             SpawnEnemyWave();
+            
+            var randomSpawnTime = Random.Range(spawnSuperEnemyRange.min, spawnSuperEnemyRange.max);
+            Invoke(nameof(SpawnSuperEnemy), randomSpawnTime);
         }
 
         private void Update()
@@ -48,6 +59,13 @@ namespace CreateWithCode.Unit4
             var z = Random.Range(-spawnRange, spawnRange);
             var spawnPosition = new Vector3(x, spawnHeight, z);
             return spawnPosition;
+        }
+        
+        private void SpawnSuperEnemy() {
+            var randomSpawnTime = Random.Range(spawnSuperEnemyRange.min, spawnSuperEnemyRange.max);
+            var spawnPosition = GenerateSpawnPosition();
+            Instantiate(superEnemyPrefab, spawnPosition, Quaternion.identity);
+            Invoke(nameof(SpawnSuperEnemy), randomSpawnTime);
         }
     }
 }
